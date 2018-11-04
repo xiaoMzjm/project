ff0aa587a7da733db088c14228189f59d826d592
zhangjiaming <383248689@qq.com>
2018-11-04 23:10:27 +0800

    base project

---
+++ base/base/src/main/java/com/zjm/base/api/BaseResource.java
@@ -0,0 +1,26 @@
+package com.zjm.base.api;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * http://localhost:8088/swagger-ui.html
+ * @author:黑绝
+ * @date:2018/11/4 下午9:59
+ */
+@RestController
+@RequestMapping("/base/resource")
+public class BaseResource {
+
+
+    @ApiOperation(value = "hello接口" , notes = "hello")
+    @ApiImplicitParam(value = "姓名" , name = "name" , required = true , dataType = "string")
+    @RequestMapping(value = "/hello/{name}" , method = RequestMethod.POST)
+    public String hello(@PathVariable String name){
+        return "hello " + name ;
+    }
+}
---
+++ thread/thread/src/main/java/com/zjm/thread/config/SwaggerConfig.java
@@ -0,0 +1,12 @@
+package com.zjm.thread.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author:黑绝
+ * @date:2018/11/4 下午8:01
+ */
+@Configuration
+public class SwaggerConfig {
+
+}
